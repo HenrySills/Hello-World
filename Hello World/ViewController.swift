8aa87e0aeff61362ec7845b039b5d02ac572224a
Henry <hes899@mail.missouri.edu>
2019-06-07 16:13:34 -0500

    First commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Henry Sills on 6/7/19.
+//  Copyright © 2019 Henry Sills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
